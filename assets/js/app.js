2685782e374fe80504dcd940f44aed01dd1424f9
Edson Camargo <dinhocmenezes@hotmail.com>
2021-07-22 01:12:06 -0300

    FIX: Custom mouse position
    Fixed custom mouse centered position.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ document.addEventListener('mousemove', e => {
     if ((e.pageY > helloY && e.pageY < helloY + hello.clientHeight) &&
         (e.pageX > helloX && e.pageX < helloX + hello.clientWidth)) {
         document.querySelector('body').setAttribute('style', 'cursor: none');
-        cursor.setAttribute('style', 'top: ' + (e.pageY - 10) + 'px; left: ' + (e.pageX - 10) + 'px; display: inline-block;');
+        cursor.setAttribute('style', 'top: ' + (e.pageY - 40) + 'px; left: ' + (e.pageX - 40) + 'px; display: inline-block;');
     } else {
         document.querySelector('body').setAttribute('style', 'cursor: auto');
         cursor.setAttribute('style', 'top: auto; left: auto;');
